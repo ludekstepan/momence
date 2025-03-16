d4fb3874d3cfe20d5d2aab3b7760a04dfcf76daf
Ludek Stepan <ludek.stepan@gmail.com>
2025-03-16 22:24:23 +0100

    aws backend

--- amplify/my-first-function/resource.ts
+++ amplify/my-first-function/resource.ts
@@ -2,5 +2,6 @@ import { defineFunction } from "@aws-amplify/backend";
 
 export const myFirstFunction = defineFunction({
   name: "my-first-function",
+  runtime: 22, // Node v22
   entry: "./handler.ts"
 });
