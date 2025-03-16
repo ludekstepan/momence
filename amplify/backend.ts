1b6b6258bf1d5054982c99bbb567106cf4ca90d4
Ludek Stepan <ludek.stepan@gmail.com>
2025-03-16 23:36:34 +0100

    aws backend

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -3,11 +3,26 @@ import { auth } from './auth/resource';
 import { data } from './data/resource';
 import { myFirstFunction } from './my-first-function/resource'
 
+import { Stack } from "aws-cdk-lib";
+import {
+  AuthorizationType,
+  CognitoUserPoolsAuthorizer,
+  Cors,
+  LambdaIntegration,
+  RestApi,
+} from "aws-cdk-lib/aws-apigateway";
+
+
+
+
 /**
  * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
  */
-defineBackend({
+const backend = defineBackend({
   // auth,
   // data,
   myFirstFunction
 });
+
+// create a new API stack
+const apiStack = backend.createStack("api-stack");
